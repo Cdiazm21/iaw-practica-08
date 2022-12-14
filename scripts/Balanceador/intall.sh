d651ec6a8652a3e46b76b6c10b809e5729e6e254
Carlos <holao@electronico.com>
2022-12-14 12:26:34 +0000

    error balanceador

--- scripts/Balanceador/intall.sh
+++ scripts/Balanceador/intall.sh
@@ -15,13 +15,6 @@ apt-get upgrade -y
 # Instalamos el servidor apache2
 apt-get install apache2 -y
 
-# Instalamos los modulos php
-
-apt-get install php libapache2-mod-php php-mysql -y
-
-# copiamos el archivo de phpinfo de php
-cp ../php/info.php /var/www/html
-
 # Borramos el archivo de index.html
 rm -f /var/www/html/index.html
 
