8743420fb9b12f055e93cd28ea2c049ed3e032ea
Carlos <cdiazmaguadulce@gmail.com>
2022-12-16 08:57:48 +0100

    Organizacion de scripts

---
+++ scripts/sh/Frontend_1/install_fronted1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+# Actualizamos los repositorios
+apt-get update
+
+# Actualizamos los paquetes
+apt-get upgrade -y
+
+# Instalamos el servidor apache2
+apt-get install apache2 -y
+
+# Instalamos los modulos php
+
+apt-get install php libapache2-mod-php php-mysql -y
+
+# copiamos el archivo de phpinfo de php
+cp ../../../php/info.php /var/www/html
+
+# Borramos el archivo de index.html
+rm -f /var/www/html/index.html
